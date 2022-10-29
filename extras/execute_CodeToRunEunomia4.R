449be58974cd8507970624015d2dc31a4b1bba2b
Solon Ioannou <sioann88@gmail.com>
2022-10-29 10:38:51 +0200

    - adding CodeToRunEunomia scripts to run from command line

---
+++ extras/execute_CodeToRunEunomia4.R
@@ -0,0 +1,13 @@
+outcomeId = 3
+outcomeName = "gi"
+databaseName = "Eunomia"
+rscriptLoc =file.path(getwd(), "extras/CodeToRunEunomia4.R")
+inputDir = file.path(getwd(), "data", "Eunomia6")
+outputDir = file.path(getwd(), "Eunomia8")
+
+# rscriptLoc = file.path(getwd(), "experimentation/testing_optparse.R")
+
+# system(paste("Rscript", rscriptLoc, "-o", outcomeId, "-c", outcomeName,"-d", databaseName ,"-i", inputDir ))
+system(paste("Rscript", rscriptLoc, outcomeId,outcomeName, databaseName ,inputDir, outputDir))
+
+
