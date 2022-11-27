e017ef6663a380ffabc00a23f9486c49d53b491b
Solon Ioannou <sioann88@gmail.com>
2022-11-27 13:58:43 +0100

    Adding correction for arulesSequences misread

--- R/mineFrequentPatterns.R
+++ R/mineFrequentPatterns.R
@@ -177,6 +177,10 @@ mineTotalFrequentPatterns <- function(trainData,
                                                    maxsize = itemSize),
                                   control = list(verbose = TRUE, tidLists = TRUE))
     
+    # The following is to correct for the arulesSequences misreading of rowIds 
+    tidLists <- supportingTransactions(s0, transactions = transactions)
+    s0@tidLists <- tidLists
+    
     nameMinSup <- gsub(pattern = "\\.", replacement = "_", x = minimumSupport)
     namePatternLength <- as.numeric(patternLength)
     # nameLookBackPeriod <- max(inputData$timeId)
