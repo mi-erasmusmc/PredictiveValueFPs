45c4a47cc2a86b41bf32916bc0849655e65bebb0
Solon Ioannou <sioann88@gmail.com>
2022-12-06 11:55:08 +0100

    - removed referencing of package's own functions. - improved style of text.

--- R/Main.R
+++ R/Main.R
@@ -1,55 +1,53 @@
 #' @export
-execute <- function(runExtractAtemporalData = FALSE, 
-                    runExtractTemporalData = FALSE, 
-                    runPrepareData = FALSE, 
-                    runExtractFPs = FALSE, 
-                    runGenerateFPObjects = FALSE, 
+execute <- function(runExtractAtemporalData = FALSE,
+                    runExtractTemporalData = FALSE,
+                    runPrepareData = FALSE,
+                    runExtractFPs = FALSE,
+                    runGenerateFPObjects = FALSE,
                     runBaseline = FALSE,
-                    runPrediction = FALSE, 
+                    runPrediction = FALSE,
                     runRecalibration = FALSE,
                     runPlpSettings,
-                    runFrequentPatternsSettings, 
+                    runFrequentPatternsSettings,
                     analysisSettings,
                     covariateSettings,
-                    saveDirectory){
-  
-  
+                    saveDirectory) {
   # Settings required for running plp
-  populationSettings = runPlpSettings$populationSettings
-  splitSettings = runPlpSettings$splitSettings
-  sampleSettings = runPlpSettings$sampleSettings
-  preprocessSettings = runPlpSettings$preprocessSettings
-  modelSettings = runPlpSettings$modelSettings
-  logSettings = runPlpSettings$logSettings
-  executeSettings = runPlpSettings$executeSettings
-  
+  populationSettings <- runPlpSettings$populationSettings
+  splitSettings <- runPlpSettings$splitSettings
+  sampleSettings <- runPlpSettings$sampleSettings
+  preprocessSettings <- runPlpSettings$preprocessSettings
+  modelSettings <- runPlpSettings$modelSettings
+  logSettings <- runPlpSettings$logSettings
+  executeSettings <- runPlpSettings$executeSettings
+
   # Settings required for running FPM
-  temporalPlpData = runFrequentPatternsSettings$temporalPlpData
-  minMinimumSupport = min(runFrequentPatternsSettings$minimumSupportValues)
-  maxPatternLength = max(runFrequentPatternsSettings$patternLengthValues)
-  minimumSupportValues = runFrequentPatternsSettings$minimumSupportValues
-  patternLengthValues = runFrequentPatternsSettings$patternLengthValues
-  
+  temporalPlpData <- runFrequentPatternsSettings$temporalPlpData
+  minMinimumSupport <- min(runFrequentPatternsSettings$minimumSupportValues)
+  maxPatternLength <- max(runFrequentPatternsSettings$patternLengthValues)
+  minimumSupportValues <- runFrequentPatternsSettings$minimumSupportValues
+  patternLengthValues <- runFrequentPatternsSettings$patternLengthValues
+
   # Settings for analysis
-  outcomeId = analysisSettings$outcomeId
-  analysisId = analysisSettings$analysisId
-  analysisName = analysisSettings$analysisName 
-  atemporalPlpData = analysisSettings$atemporalPlpData
-  fileName = stringr::str_remove(analysisName, "predicting_")
-  covariateSet = analysisSettings$covariateSet
-  
+  outcomeId <- analysisSettings$outcomeId
+  analysisId <- analysisSettings$analysisId
+  analysisName <- analysisSettings$analysisName
+  atemporalPlpData <- analysisSettings$atemporalPlpData
+  fileName <- stringr::str_remove(analysisName, "predicting_")
+  covariateSet <- analysisSettings$covariateSet
+
   # Settings for extracting covariateData
-  databaseDetails = covariateSettings$databaseDetails 
-  atemporalCovariateSettings = covariateSettings$atemporalCovariateSettings
-  temporalCovariateSettings = covariateSettings$temporalCovariateSettings
-  restrictPlpDataSettings = covariateSettings$restrictPlpDataSettings
-  covariateDirectory = covariateSettings$saveDirectory
-  
-  if (!dir.exists(saveDirectory)){
+  databaseDetails <- covariateSettings$databaseDetails
+  atemporalCovariateSettings <- covariateSettings$atemporalCovariateSettings
+  temporalCovariateSettings <- covariateSettings$temporalCovariateSettings
+  restrictPlpDataSettings <- covariateSettings$restrictPlpDataSettings
+  covariateDirectory <- covariateSettings$saveDirectory
+
+  if (!dir.exists(saveDirectory)) {
     dir.create(saveDirectory, recursive = TRUE)
   }
-  
-  
+
+
   ########
   # saveDirectory <-  file.path(saveDirectory, analysisId)
   baskets_directory <- file.path(saveDirectory, analysisId, "data", "inputs", "baskets")
@@ -59,69 +57,85 @@ execute <- function(runExtractAtemporalData = FALSE,
   # plpInput_directory <- file.path(saveDirectory, analysisId, "data", "inputs", "predictorSets", fileName)
   plpData_directory <- file.path(saveDirectory, analysisId, "data", "inputs", "plpData")
   plpOutput_directory <- file.path(saveDirectory, analysisId, "results")
-  
+
   # Step 0: extract covariates
-  if (runExtractAtemporalData){
-    PredictiveValueFPs::extractAtemporalData(databaseDetails = databaseDetails, 
-                                             covariateSettings = atemporalCovariateSettings, 
-                                             restrictPlpDataSettings = restrictPlpDataSettings, 
-                                             outputFolder = file.path(covariateDirectory, analysisId), 
-                                             fileName = fileName)
+  if (runExtractAtemporalData) {
+    extractAtemporalData(
+      databaseDetails = databaseDetails,
+      covariateSettings = atemporalCovariateSettings,
+      restrictPlpDataSettings = restrictPlpDataSettings,
+      outputFolder = file.path(covariateDirectory, analysisId),
+      fileName = fileName
+    )
   }
-  
-  if (runExtractTemporalData){
-    PredictiveValueFPs::extractTemporalData(databaseDetails = databaseDetails, 
-                                            covariateSettings = temporalCovariateSettings, 
-                                            restrictPlpDataSettings = restrictPlpDataSettings, 
-                                            outputFolder = file.path(covariateDirectory, analysisId), 
-                                            fileName = fileName) 
+
+  if (runExtractTemporalData) {
+    extractTemporalData(
+      databaseDetails = databaseDetails,
+      covariateSettings = temporalCovariateSettings,
+      restrictPlpDataSettings = restrictPlpDataSettings,
+      outputFolder = file.path(covariateDirectory, analysisId),
+      fileName = fileName
+    )
   }
-  
-  #step1 : pepare data with folds(for train/test) + additional recipes (sample data, tidy covariates)
-  if (runPrepareData){
-    PredictiveValueFPs::prepareData(runPlpSettings = runPlpSettings,
-                                    analysisSettings = analysisSettings, 
-                                    outputFolder = saveDirectory, 
-                                    inputFolder = covariateDirectory)
+
+  # step1 : pepare data with folds(for train/test) + additional recipes (sample data, tidy covariates)
+  if (runPrepareData) {
+    prepareData(
+      runPlpSettings = runPlpSettings,
+      analysisSettings = analysisSettings,
+      outputFolder = saveDirectory,
+      inputFolder = covariateDirectory
+    )
   }
-  
-  #step2: Mine frequent patterns using the minimum minSup value and the maximum pattern length
-  if (runExtractFPs)  {
-    PredictiveValueFPs::extractFPs(runFrequentPatternsSettings = runFrequentPatternsSettings,
-                                   outputFolder = file.path(saveDirectory, analysisId), 
-                                   inputFolder = file.path(covariateDirectory, analysisId),
-                                   fileName = fileName)
+
+  # step2: Mine frequent patterns using the minimum minSup value and the maximum pattern length
+  if (runExtractFPs) {
+    extractFPs(
+      runFrequentPatternsSettings = runFrequentPatternsSettings,
+      outputFolder = file.path(saveDirectory, analysisId),
+      inputFolder = file.path(covariateDirectory, analysisId),
+      fileName = fileName
+    )
   }
-  
-  #step3: Prepare all sets of frequent patterns that are going to be examined
-  if (runGenerateFPObjects){
-    PredictiveValueFPs::generateFPObjects(minimumSupportValues = minimumSupportValues, 
-                                          patternLengthValues = patternLengthValues, 
-                                          covariateSet = covariateSet,
-                                          outputFolder = file.path(saveDirectory, analysisId),
-                                          fileName = fileName)
+
+  # step3: Prepare all sets of frequent patterns that are going to be examined
+  if (runGenerateFPObjects) {
+    generateFPObjects(
+      minimumSupportValues = minimumSupportValues,
+      patternLengthValues = patternLengthValues,
+      covariateSet = covariateSet,
+      outputFolder = file.path(saveDirectory, analysisId),
+      fileName = fileName
+    )
   }
-  
-  #step4: Predict
-  if (runBaseline){
-    PredictiveValueFPs::predictBaseline(runPlpSettings = runPlpSettings, 
-                                        analysisSettings = analysisSettings, 
-                                        outputFolder = saveDirectory, 
-                                        inputFolder = file.path(covariateDirectory, analysisId))
+
+  # step4: Predict
+  if (runBaseline) {
+    PredictiveValueFPs::predictBaseline(
+      runPlpSettings = runPlpSettings,
+      analysisSettings = analysisSettings,
+      outputFolder = saveDirectory,
+      inputFolder = file.path(covariateDirectory, analysisId)
+    )
   }
-  
-  if (runPrediction){
-    PredictiveValueFPs::predictFPs(runPlpSettings = runPlpSettings, 
-                                   analysisSettings = analysisSettings, 
-                                   covariateSet = covariateSet,
-                                   outputFolder = saveDirectory, 
-                                   inputFolder = file.path(covariateDirectory, analysisId))
+
+  if (runPrediction) {
+    predictFPs(
+      runPlpSettings = runPlpSettings,
+      analysisSettings = analysisSettings,
+      covariateSet = covariateSet,
+      outputFolder = saveDirectory,
+      inputFolder = file.path(covariateDirectory, analysisId)
+    )
   }
-  
-  if (runRecalibration){
-    PredictiveValueFPs::recalibrateProbabilitites(runPlpSettings = runPlpSettings, 
-                                                  analysisSettings = analysisSettings, 
-                                                  outputFolder = saveDirectory, 
-                                                  inputFolder = file.path(covariateDirectory, analysisId))
+
+  if (runRecalibration) {
+    recalibrateProbabilities(
+      runPlpSettings = runPlpSettings,
+      analysisSettings = analysisSettings,
+      outputFolder = saveDirectory,
+      inputFolder = file.path(covariateDirectory, analysisId)
+    )
   }
 }
