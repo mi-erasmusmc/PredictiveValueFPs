a773a70ca99f07e13ea1f06a72cc4bc96f27ba01
Solon Ioannou <sioann88@gmail.com>
2023-01-02 15:34:04 +0200

    Added flag for recognisign if object is of emergent patterns.

--- R/generateFPObjects.R
+++ R/generateFPObjects.R
@@ -15,13 +15,13 @@ generateFPObjects <- function(minimumSupportValues,
   
   combs <- expand.grid(minimumSupportValues, patternLengthValues) %>%
     slice(n = 1:n() - 1)
-
   
+  referenceNames <- output2$plpData$Train$covariateData$covariateRef %>% dplyr::collect() %>% base::colnames()
+
+  if (!(c("support.x") %in% referenceNames)){
   if (covariateSet == "freqPatOnly"){
     covList <- list()
     for (i in seq_along(1:nrow(combs))) {
-      
-      
       
       result = list(
         population = output2$population
@@ -56,6 +56,48 @@ generateFPObjects <- function(minimumSupportValues,
         saveBakedData(object = result, file = file.path(plpData_directory, paste0(fileName, "_MS_", nameMinSup, "_PL_", namePatternLength, "_plpData")))
         # covList[[i]] <- result
       }
+  }
+  } else {
+    if (covariateSet == "freqPatOnly"){
+      covList <- list()
+      for (i in seq_along(1:nrow(combs))) {
+        
+        
+        
+        result = list(
+          population = output2$population
+        )
+        
+        result$plpData <- filterPlpDataEmergentPatterns(plpData = output2$plpData, 
+                                        minimumSupport = combs[i, 1], 
+                                        patternLength = combs[i, 2],
+                                        createSets = "freqPatsOnly") 
+        
+        nameMinSup = gsub(x = combs[i, 1], pattern = "\\.",replacement =  "_")
+        namePatternLength = combs[i, 2]
+        saveBakedData(object = result, file = file.path(plpData_directory, paste0(fileName, "_MS_", nameMinSup, "_PL_", namePatternLength, "_plpData_fpsOnly")))
+        # covList[[i]] <- result
+      }
+    }
+    if (covariateSet == "mix"){
+      covList <- list()
+      for (i in seq_along(1:nrow(combs))) {
+        result = list(
+          population = output2$population
+        )
+        
+        result$plpData <- filterPlpDataEmergentPatterns(plpData = output2$plpData, 
+                                        minimumSupport = combs[i, 1], 
+                                        patternLength = combs[i, 2],
+                                        createSets = "mix") 
+        
+        
+        nameMinSup = gsub(x = combs[i, 1], pattern = "\\.",replacement =  "_")
+        namePatternLength = combs[i, 2]
+        saveBakedData(object = result, file = file.path(plpData_directory, paste0(fileName, "_MS_", nameMinSup, "_PL_", namePatternLength, "_plpData")))
+        # covList[[i]] <- result
+      }
+    }
   }
   ParallelLogger::logInfo(paste("Done."))
 }
